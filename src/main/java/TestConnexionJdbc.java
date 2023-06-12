54fca9f36da48fbf0502ebfcd3fa3f59d29040a1
dijialei <dijialei0605@gmail.com>
2023-06-12 15:34:30 +0200

    JDBC tp02

--- src/main/java/TestConnexionJdbc.java
+++ src/main/java/TestConnexionJdbc.java
@@ -1,14 +1,25 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.ResourceBundle;
 
 public class TestConnexionJdbc {
+    private static final String USERNAME;
+    private static final String PASSWORD;
+    private static final String URL;
+
+    static {
+        ResourceBundle bundle = ResourceBundle.getBundle("database");
+        USERNAME = bundle.getString("username");
+        PASSWORD = bundle.getString("password");
+        URL = bundle.getString("url");
+    }
+
     public static void main(String[] args) {
-        String username = "root";
-        String passwork = "admin";
-        String url = "jdbc:mysql://127.0.0.1:3306/compta";
+
+
         try {
-            Connection connection = DriverManager.getConnection(url,username,passwork);
+            Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
             System.out.println(connection);
         } catch (SQLException e) {
             throw new RuntimeException(e);
